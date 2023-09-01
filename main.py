4fa36279dbd758f96c3e41fc3ba36cbf0c8557fe
Steven Bouldin <96607187+Sacred-G@users.noreply.github.com>
2023-09-01 16:24:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -79,12 +79,12 @@ def main():
     # Data manipulations
  
     
-       if 'your_column' in data.columns:
-            data.dropna(subset=['your_column'], inplace=True) 
-            data['your_column'] = data['your_column'].astype(float)
-        else:
-            st.error(f"The column 'your_column' does not exist. Available columns are: {data.columns.tolist()}")
-    
+           if 'your_column' in data.columns:
+                data.dropna(subset=['your_column'], inplace=True) 
+                data['your_column'] = data['your_column'].astype(float)
+            else:
+                st.error(f"The column 'your_column' does not exist. Available columns are: {data.columns.tolist()}")
+        
 
         
         if st.button("Generate Chart"):
